d3e5531f8dc8a5f959eab350f8d3405e5a6162e7
slapjd <key90011337@gmail.com>
2018-07-12 18:45:15 +0100

    Add project files.

---
+++ MultiThreadChat/MainWindow.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+using MultiThreadChat.Networking;
+
+namespace MultiThreadChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _txtSendDefaultText;
+        private bool _txtSendWasModified = false;
+        private ExampleClient _client;
+        private NetServer<ExampleClient> _server;
+        private bool _skipTextChangedEvent = true; //The event is loaded once at startup and the function needs to be skipped then
+
+        /// <summary>
+        /// Constructor.
+        /// Attempts to automatically set the IP Address box to the local ip address
+        /// and initialises the _txtSendDefaultText value to whatever is in txtSend by default
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IPAddress[] ipAddresses = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress address in ipAddresses)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    txtServerIP.Text = address.ToString();
+                }
+            }
+
+            _txtSendDefaultText = txtSend.Text;
+        }
+
+        /// <summary>
+        /// Runs whenever the local client disconnects
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Arguments associated with client disconnects</param>
+        private void _clientDisconnected(object sender, DisconnectArgs e)
+        {
+            _client = null;
+
+            if (_server == null)
+            {
+                btnServerStart.IsEnabled = true;
+            }
+            btnServerConnect.IsEnabled = true;
+            btnServerDisconnect.IsEnabled = false;
+
+            txtSend.Text = "";
+            txtSend_ChangeTextWithKeyboardFocus(); //See _sendMessage for details
+            txtSend.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Runs whenever the local server is shut down
+        /// </summary>
+        /// <param name="sender">The object that fired the event</param>
+        private void _serverShutdown(object sender)
+        {
+            _server = null;
+            btnServerStart.IsEnabled = true;
+            btnServerStop.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Fired whenever a message is sent/received. Adds the message to txtLog
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Arguments associated with a message event</param>
+        private void _logMessage(object sender, MsgEventArgs e)
+        {
+            string _message = Encoding.Unicode.GetString(e.Message);
+            txtLog.AppendText(_message + Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Tells the local client to send a message from txtSend
+        /// </summary>
+        private void _sendMessage()
+        {
+            if (_txtSendWasModified)
+            {
+                string _message = "<" + txtUsername.Text + "> " + txtSend.Text;
+                byte[] _messageBuffer = Encoding.Unicode.GetBytes(_message);
+
+                _client.SendAsync(_messageBuffer);
+
+                //Remember that the textchanged event shouldn't be skipped here (we're resetting the textbox)
+                txtSend.Text = "";
+                //Sometimes this is called before TextChanged so I need to run it again
+                txtSend_ChangeTextWithKeyboardFocus();
+            }
+        }
+
+        /// <summary>
+        /// Changes the current value of txtSend based on keyboard focus on txtSend.
+        /// Used to provide the disappearing "Enter Message" text
+        /// </summary>
+        private void txtSend_ChangeTextWithKeyboardFocus()
+        {
+            if (txtSend.IsKeyboardFocused == true)
+            {
+                if (!_txtSendWasModified)
+                {
+                    _skipTextChangedEvent = true;
+                    txtSend.Text = "";
+                }
+            }
+            else if (txtSend.Text == "")
+            {
+                _skipTextChangedEvent = true;
+                txtSend.Text = _txtSendDefaultText;
+            }
+        }
+
+        /// <summary>
+        /// Fired whenever the focus is changed on txtSend (focused or unfocused)
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void txtSend_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            txtSend_ChangeTextWithKeyboardFocus();
+        }
+
+        /// <summary>
+        /// Fired whenever a key is pushed down and txtSend is focused
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) //Send a message when enter is pushed
+            {
+                _sendMessage();
+            }
+        }
+
+        /// <summary>
+        /// Fired whenever the text changes in txSend.
+        /// Some functions are skipped based on a flag in MainWindow
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void txtSend_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!_skipTextChangedEvent)
+            {
+                if (txtSend.Text == "")
+                {
+                    _txtSendWasModified = false;
+                    btnSend.IsEnabled = false;
+                }
+                else
+                {
+                    _txtSendWasModified = true;
+                    btnSend.IsEnabled = true;
+                }
+            }
+            else
+            {
+                _skipTextChangedEvent = false;
+            }
+        }
+
+        /// <summary>
+        /// Fired when btnServerStart is clicked
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnServerStart_Click(object sender, RoutedEventArgs e)
+        {
+            _server = new NetServer<ExampleClient>(Convert.ToInt32(txtServerPort.Text), t => new ExampleClient(t));
+            _server.ServerShutdown += _serverShutdown;
+
+            btnServerStart.IsEnabled = false;
+            btnServerStop.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Fired when btnServerConnect is clicked
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnServerConnect_Click(object sender, RoutedEventArgs e)
+        {
+            //_client = new NetClient(new IPEndPoint(IPAddress.Parse(txtServerIP.Text), Convert.ToInt32(txtServerPort.Text)));
+            _client = new ExampleClient(new IPEndPoint(IPAddress.Parse(txtServerIP.Text), Convert.ToInt32(txtServerPort.Text)));
+            btnServerConnect.IsEnabled = false;
+            btnServerStart.IsEnabled = false;
+            btnServerDisconnect.IsEnabled = true;
+
+            txtSend.IsEnabled = true;
+
+            _client.MessageReceived += _logMessage;
+            _client.MessageSent += _logMessage;
+            _client.Disconnected += _clientDisconnected;
+        }
+
+        /// <summary>
+        /// Fired when btnServerStop is clicked
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnServerStop_Click(object sender, RoutedEventArgs e)
+        {
+            _server.Shutdown();
+        }
+
+        /// <summary>
+        /// Fired when btnServerDisconnect is clicked
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnServerDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            _client.Disconnect(new DisconnectArgs("Local disconnect requested"));
+        }
+
+        /// <summary>
+        /// Fired when btnSend is clicked
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            _sendMessage();
+        }
+    }
+}
---
+++ MultiThreadChat/Networking/NetServer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+using System.Diagnostics;
+
+namespace MultiThreadChat.Networking
+{
+    delegate void ClientConnectedHandler(object sender);
+    delegate void ServerShutdownHandler(object sender);
+
+    class NetServer<T> where T : NetClient
+    {
+        /// <summary>
+        /// Listener object. Listens for new clients
+        /// </summary>
+        protected TcpListener _server;
+        /// <summary>
+        /// List of all connected clients
+        /// </summary>
+        protected List<T> _clients;
+        /// <summary>
+        /// Constructor to use when making new client objects.
+        /// Works around the fact that generic types cannot be restricted by constructors with parameters
+        /// </summary>
+        protected Func<TcpClient, T> _clientConstructor;
+        /// <summary>
+        /// Holds whether the server should be running, or if it's shutting down/shut down
+        /// </summary>
+        protected bool _serverRunning;
+
+        public event ClientConnectedHandler ClientConnected;
+        public event ServerShutdownHandler ServerShutdown;
+
+        protected void _invokeClientConnected()
+        {
+            ClientConnected?.Invoke(this);
+        }
+        protected void _invokeServerShutdown()
+        {
+            ServerShutdown.Invoke(this);
+        }
+
+        /// <summary>
+        /// Constantly listens for new clients until _serverRunning is false (server is shutting down)
+        /// </summary>
+        protected virtual async void _newClientLoop()
+        {
+            while (_serverRunning)
+            {
+                _server.Start();
+                try
+                {
+                    TcpClient newTcpClient = await _server.AcceptTcpClientAsync();
+
+                    var newClient = _clientConstructor(newTcpClient);
+                    _clients.Add(newClient);
+
+                    newClient.MessageReceived += _forwardMessage;
+                    newClient.Disconnected += _clientDisconnected;
+
+                    ClientConnected?.Invoke(this);
+                }
+                catch (ObjectDisposedException) //Because the listener was stopped which threw this
+                {
+                    if (_serverRunning) //If this exception happens while the server is running, we have problems
+                    {
+                        throw;
+                    }
+                }//*/
+            }
+        }
+
+        /// <summary>
+        /// Sends a message to all clients except the client that sent the message.
+        /// Mostly used when a message is received
+        /// </summary>
+        /// <param name="sender">Object that sent the message</param>
+        /// <param name="e">Arguments associated with a message event</param>
+        protected virtual void _forwardMessage(object sender, MsgEventArgs e)
+        {
+            foreach (var _client in _clients)
+            {
+                if (_client != sender)
+                {
+                    _client.SendAsync(e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Runs when any client disconnects.
+        /// Removes the client from _clients
+        /// </summary>
+        /// <param name="sender">Object that fired the event</param>
+        /// <param name="e">Arguments associated with disconnect event</param>
+        protected virtual void _clientDisconnected(object sender, DisconnectArgs e)
+        {
+            var netSender = sender as T;
+            _clients.Remove(netSender);
+        }
+
+        /// <summary>
+        /// Sends a message to all currently connected clients
+        /// </summary>
+        /// <param name="Message">Message to send</param>
+        protected virtual void Broadcast(byte[] Message)
+        {
+            foreach (var client in _clients)
+            {
+                client.SendAsync(Message);
+            }
+        }
+
+        /// <summary>
+        /// Runs server shutdown procedures (disconnects all clients)
+        /// </summary>
+        protected virtual void Shutdown()
+        {
+            _serverRunning = false;
+            _server.Stop();
+
+            var _clientsCopy = new List<T>(_clients); //So I can run a foreach loop without stuff being removed in the middle
+            foreach (var _client in _clientsCopy)
+            {
+                _client.Disconnect(new DisconnectArgs("Server shutdown"));
+                //Removal of clients from _clients is handled by _clientDisconnected
+            }
+
+            ServerShutdown?.Invoke(this);
+        }
+
+        /// <summary>
+        /// Starts up a TCP server (listener)
+        /// </summary>
+        /// <param name="Port">Port that the server listens on</param>
+        /// <param name="Constructor">
+        /// Constructor for the client class that takes a TcpClient parameter.
+        /// For example, providing something like (t) => new T(t) where T is the client's type would work as long as new T(t) accepts a TcpClient.
+        /// </param>
+        public NetServer(int Port, Func<TcpClient,T> Constructor)
+        {
+            _server = new TcpListener(IPAddress.Any, Port);
+            _clients = new List<T>();
+            _clientConstructor = Constructor;
+
+            _serverRunning = true;
+            _newClientLoop();
+        }
+    }
+}
